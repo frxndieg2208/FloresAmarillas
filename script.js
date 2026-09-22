ffb55f6bf07f9ba760688c36dd8265f4044756c8
Frank <frankdiegoqp@gmail.com>
2026-09-21 19:56:11 -0500

    V1.0 Flores Amarillas

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrirSorpresa() {
+
+    // Ocultar la pantalla inicial
+    document.getElementById("inicio").style.display = "none";
+
+
+    // Mostrar la sorpresa
+    document.getElementById("sorpresa").style.display = "block";
+
+}
